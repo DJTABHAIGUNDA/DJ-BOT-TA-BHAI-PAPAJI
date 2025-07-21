17355b1d4e1cccd66ae87c9a2c854db1ed1551ce
DJTABHAIGUNDA <srkbhaidjtabhaigundapapaji@gmail.com>
2025-07-21 12:08:43 +0530

    Update OWNER.js

--- Priyansh/commands/OWNER.js
+++ Priyansh/commands/OWNER.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("virat") || 
 react.includes("owner")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + ❤️...𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢...❤️ ● ──────────────── ●𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢 ● ──────────────── ●𝐎𝐰𝐧𝐞𝐫 ➻❤️....𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢....❤️",attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpeg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿 + ❤️...𝐃𝗝 🅣︎🄰🅑︎🄷🅐︎🄸...❤️ ● ──────────────── ●👺☠️𝗗𝗝 𝐓𝐀𝐁𝐇𝐀𝐈🥷● ──────────────── ●𝐎𝐰𝐧𝐞𝐫 ➻❤️....𝐃𝗝 🅣︎🄰🅑︎🄷🅐︎🄸....❤️",attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpeg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
